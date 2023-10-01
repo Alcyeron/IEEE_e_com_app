5c39ce5b1ea0d4483a37a55e8b504dc301b3ad8e
Dhruv Deshpande <f20230588@pilani.bits-pilani.ac.in>
2023-10-01 16:58:18 +0530

    Add basic UI features (incomplete)

--- shop/views.py
+++ shop/views.py
@@ -1,7 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
+items = [
+    {
+        'name' : "MI Power Bank 3i 20000mAh Lithium Polymer 18W Fast Power Delivery Charging",
+        'rating' : 4.2,
+        'price' : 2149,
+        'mrp' : 2199,
+        'discount' : 2
+    },
+    {
+        'name' : "MI 10000mAh Lithium Ion, Lithium Polymer Power Bank Pocket Pro with 22.5 Watt Fast Charging",
+        'rating' : 4.3,
+        'price' : 1699,
+        'mrp' : 2499,
+        'discount' : 32
+    }
+]
 def shop(request):
     return render(request, "shop/shop.html")
 def products(request):
-    return render(request, "shop/products.html")
+    content = {
+        'title': "Products",
+        'items' : items
+    }
+    return render(request, "shop/products.html", content)
